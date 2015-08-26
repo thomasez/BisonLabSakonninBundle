2de54e4bd17aa7786590bf299ec193ebbf230218
Thomas Lundquist <thomasez@bisonlab.no>
2015-08-26 12:45:25 +0200

    Adding a postMessage function. Time to start posting messages.

--- Service/Messages.php
+++ Service/Messages.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use BisonLab\SakonninBundle\Entity\Message;
+use BisonLab\SakonninBundle\Entity\MessageContext;
 
 /**
  * Messages service.
@@ -16,10 +17,42 @@ class Messages
 {
 
     private $container;
+    private $entityManager;
 
     public function __construct($container)
     {
         $this->container         = $container;
     }
 
+    public function postMessage($data, $context_data)
+    {
+        $em = $this->_getManager();
+        $message = new Message($data);
+
+        if (isset($context)
+            && isset($context['system'])
+            && isset($context['object_name'])
+            && isset($context['external_id'])) {
+
+            $message_context = new MessageContext();
+            $message->addContext($message_context);
+            $message_context->setSystem($context['system']);
+            $message_context->setObjectName($context['object_name']);
+            $message_context->setExternalId($context['external_id']);
+            $em->persist($message_context);
+        }
+        $em->persist($message);
+        $this->flush();
+
+        return ($message);
+    }
+
+    private function _getManager()
+    {
+        if (!$this->entityManager) {
+            $this->entityManager 
+                = $this->container->get('doctrine')->getManager();
+        }
+        return $this->entityManager;
+    } 
 }
