fc5df64efa7bce02c83cf25e4002d4df4e4b5b7f
Thomas Lundquist <thomasez@bisonlab.no>
2015-08-27 14:57:37 +0200

    Making contexts work and shown.

--- Service/Messages.php
+++ Service/Messages.php
@@ -37,16 +37,16 @@ dump($message_type);
             throw new \InvalidArgumentException("No message type found or set.");
         }
 
-        if (isset($context)
-            && isset($context['system'])
-            && isset($context['object_name'])
-            && isset($context['external_id'])) {
+        if (isset($context_data)
+            && isset($context_data['system'])
+            && isset($context_data['object_name'])
+            && isset($context_data['external_id'])) {
 
             $message_context = new MessageContext();
             $message->addContext($message_context);
-            $message_context->setSystem($context['system']);
-            $message_context->setObjectName($context['object_name']);
-            $message_context->setExternalId($context['external_id']);
+            $message_context->setSystem($context_data['system']);
+            $message_context->setObjectName($context_data['object_name']);
+            $message_context->setExternalId($context_data['external_id']);
             $em->persist($message_context);
         }
 
