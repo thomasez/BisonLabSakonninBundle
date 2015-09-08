972408da1d7cac25a722ace9fecdab35036679a6
Thomas Lundquist <thomasez@bisonlab.no>
2015-09-08 14:41:40 +0200

    But, just in case..

---
+++ Form/FunctionAttributeType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BisonLab\SakonninBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FunctionAttributeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('key')
+            ->add('value')
+        ;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'function_attribute';
+    }
+}
