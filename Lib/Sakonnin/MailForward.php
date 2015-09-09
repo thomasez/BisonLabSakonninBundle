238c94d38ed87e1373b2ccbf17da91d5ea53a95f
Thomas Lundquist <thomasez@bisonlab.no>
2015-09-09 16:11:00 +0200

    Adding the URL of the message to any body.

--- Lib/Sakonnin/MailForward.php
+++ Lib/Sakonnin/MailForward.php
@@ -9,6 +9,7 @@ class MailForward
 {
 
     protected $container;
+    protected $router;
 
     public function __construct($container, $options = array())
     {
@@ -22,16 +23,31 @@ class MailForward
         error_log("Kom meg til funksjonen." . print_r($options['attributes'], true));
         $message = $options['message'];
 
+        $router = $this->getRouter();
+        $url = $router->generate('message_show', array('id' => $message->getId()), true);
+        // Not gonna do html, yet at least..
+        // $body = '<a href="' . $url . '">Link to this message</a>' . "\n\n";
+        $body = "Link to this message: " . $url  . "\n\n";
+        $body .= $message->getBody();
+
         $mail = \Swift_Message::newInstance()
         ->setSubject($message->getSubject())
         ->setFrom($message->getFrom())
         ->setTo(implode(",", $options['attributes']))
-        ->setBody($message->getBody(),
+        ->setBody($body,
             'text/plain'
         ) ;
         $this->container->get('mailer')->send($mail);
 
         return true;
+    }
+
+    public function getRouter()
+    {
+        if (!$this->router) {
+            $this->router = $this->container->get('router');
+        }
+        return $this->router;
     }
 
 }
