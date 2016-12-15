9d0ff3ee01f9a9e0fb7ff4f40bb1b824cc0f5932
Thomas Lundquist <github@bisonlab.no>
2016-12-15 10:46:51 +0100

    Now SMS sending via this old interface actually works.

--- Lib/SmsHandler/PsWinComMail.php
+++ Lib/SmsHandler/PsWinComMail.php
@@ -26,6 +26,8 @@ class PsWinComMail
         $this->smsfrom  = $options['smsfrom'];
         $this->mailfrom = $options['mailfrom'];
         $this->mailto   = $options['mailto'];
+        $this->default_country_prefix   = $options['default_country_prefix'];
+        $this->national_number_lenght   = $options['national_number_lenght'];
     }
 
     public function send($message, $receivers, $options = array())
@@ -43,6 +45,7 @@ EOMSG;
             $receivers = array($receivers);
 		
         foreach ($receivers as $number) {
+            if (strlen((string)$number) == $this->national_number_lenght) $number = $this->default_country_prefix . $number;
             $msg .= <<<EOMSG
 <MSG>
 <TEXT>$message</TEXT>
@@ -54,14 +57,18 @@ EOMSG;
 		
 		$msg .="\n</MSGLST>\n</SESSION>";
 	
-error_log($msg);
         $mail = \Swift_Message::newInstance()
         ->setSubject("SMS")
         ->setFrom($this->mailfrom)
         ->setTo($this->mailto)
         ->setBody($msg,
             'text/plain'
-        ) ;
+        );
+
+        $headers = $mail->getHeaders();
+        $headers->addTextHeader('Return-Path', $this->mailfrom);
+        $headers->addTextHeader('X-Sender', $this->mailfrom);
+
         $this->container->get('mailer')->send($mail);
     }
 }
