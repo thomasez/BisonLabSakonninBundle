2e8e81997240d98f0964f6a74e576ab4dbc0f0d7
Thomas Lundquist <thomasez@bisonlab.no>
2023-05-04 08:47:22 +0200

    Bad command.

--- src/Command/SakonninExpungeCommand.php
+++ src/Command/SakonninExpungeCommand.php
@@ -11,7 +11,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Doctrine\Persistence\ManagerRegistry;
 
-use BisonLab\SakonninBundle\Entity\MessageType as MessageType;
+use BisonLab\SakonninBundle\Entity\MessageType;
+use BisonLab\SakonninBundle\Entity\Message;
 
 /**
  * Imports message types. Either for fixtures or just install for prod.
